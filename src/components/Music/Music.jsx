a85ac84f6f2f000153a32d1a2e4182714f1e3119
oshumskyi7 <oshumskyi7@gmail.com>
2021-12-09 21:03:13 +0200

    created component music

---
+++ src/components/Music/Music.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import s from './News.module.css';
+
+const Music = () => {
+    return (
+        <div>
+            <div>
+                Music
+            </div>
+        </div>
+    )
+}
+
+export default Music;
