47bd3ada03dcb072b4030eb1d7802555aab02d1c
oshumskyi7 <oshumskyi7@gmail.com>
2021-12-09 21:31:14 +0200

    created component settings

---
+++ src/components/Settings/Settings.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import s from './Settings.module.css';
+
+const Settings = () => {
+    return (
+        <div>
+            <div>
+                Settings
+            </div>
+        </div>
+    )
+}
+
+export default Settings;
